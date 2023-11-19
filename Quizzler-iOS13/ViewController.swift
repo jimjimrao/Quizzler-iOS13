5fd4839439772d79450083c19b7e264d1f057719
Jimmy Rao <32654532+jimjimrao@users.noreply.github.com>
2023-11-18 19:12:31 -0500

    sample questions and show each question

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -10,11 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var questionLabel: UILabel!
+    
+    
+    @IBOutlet weak var falseButton: UIButton!
+    @IBOutlet weak var trueButton: UIButton!
+    
+    let quiz = [
+        "Is the sky blue?", // True
+        "Do dogs meow?", // False
+        "Is the Earth flat?" // False
+    ]
+    
+    var questionNumber = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        updateUI()
+        
     }
 
-
+    
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        questionNumber += 1
+        updateUI()
+    }
+    
+    func updateUI(){
+        questionLabel.text = quiz[questionNumber]
+    }
 }
 
